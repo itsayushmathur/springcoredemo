718208120e9ed1e44493d31d2ea3c62ecf44f29b
itsayushmathur <mathurayush121@gmail.com>
2025-07-23 11:37:14 +0530

    Bean LifeCycle Methods

--- src/main/java/com/itsayushmathur/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/itsayushmathur/springcoredemo/common/CricketCoach.java
@@ -1,16 +1,27 @@
 package com.itsayushmathur.springcoredemo.common;
-
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CricketCoach implements Coach {
 
     public CricketCoach(){
         System.out.println("In Constructor: "+ getClass().getSimpleName());
     }
+    //define our init method
+    @PostConstruct
+    public void doMyStartupStuff(){
+        System.out.println("In doMyStartupStuff: "+ getClass().getSimpleName());
+    }
+
+    //define our destroy method
+    @PreDestroy
+    public void doMyClosingStuff(){
+        System.out.println("In doMyClosingStuff: "+ getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes. ;)";
--- src/main/java/com/itsayushmathur/springcoredemo/rest/DemoController.java
+++ src/main/java/com/itsayushmathur/springcoredemo/rest/DemoController.java
@@ -10,14 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
-    private Coach anotherCoach;
 
     //Constructor Injection:
     @Autowired
-    public DemoController(@Qualifier("cricketCoach") Coach theCoach,@Qualifier("cricketCoach") Coach theAnotherCoach){
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach){
         myCoach = theCoach;
         System.out.println("In Constructor: "+ getClass().getSimpleName());
-        anotherCoach=theAnotherCoach;
     }
 
 //    //Setter Injection:
@@ -29,13 +27,6 @@ public class DemoController {
     public String getDailyWorkout(){
         return myCoach.getDailyWorkout();
     }
-
-    @GetMapping("/check")
-        public String check(){
-            return "Comparing Beans: "+ (myCoach== anotherCoach);
-        }
-
-
 
 }
 
